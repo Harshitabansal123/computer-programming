4b135b70878b35bc05d218fbeb776b0e62083fa1
Harshita Bansal <harshitaa9839@gmail.com>
2024-11-28 11:28:10 +0530

    check item  list_q03.py

---
+++ coding-assignment/list/list_q03.py
@@ -0,0 +1,9 @@
+# Define a list of numbers
+numbers = [5, 10, 15, 20, 25]
+
+
+# Check if an item exists in the list 
+if 15 in numbers:
+print("15 is in the list") 
+else:
+print("15 is not in the list")
