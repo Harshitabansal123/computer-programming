ad878f197f070e362b0a5de25642a4595fb2e6ea
Harshita Bansal <harshitaa9839@gmail.com>
2024-11-28 11:32:39 +0530

    list_q06.py

---
+++ coding-assignment/list/list_q06.py
@@ -0,0 +1,5 @@
+# Define a list of numbers
+numbers = [5, 10, 15, 20, 25]
+# Sum of all elements in the list
+sum_of_numbers = sum(numbers) 
+print("Sum of all numbers in the list:", sum_of_numbers)
