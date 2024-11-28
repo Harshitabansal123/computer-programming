8fe285b41c81238cf2644f61f4e3f1fa4dc25a92
Harshita Bansal <harshitaa9839@gmail.com>
2024-11-28 11:29:42 +0530

    list_q04.py

---
+++ coding-assignment/list/list_q04.py
@@ -0,0 +1,5 @@
+# Define a list of numbers
+numbers = [5, 10, 15, 20, 25]
+# List comprehension to create a new list with squared values
+squared_numbers = [x**2 for x in numbers] 
+print("List of squared numbers:", squared_numbers)
